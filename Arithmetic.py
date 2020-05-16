d769186cf3d0aa581d91880a7b9a95d5351b3ac8
Yotam Alon <yotamalon@gmail.com>
2020-05-16 14:01:41 +0300

    Added more functions and made them into gates with tests

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,12 +1,93 @@
 from qiskit import QuantumCircuit
 
 
-def carry(circuit: QuantumCircuit, control_qubit1, control_qubit2, control_qubit3, target_qubit):
-    circuit.ccx(control_qubit3, control_qubit1, target_qubit)
-    circuit.cx(control_qubit2, control_qubit1)
-    circuit.ccx(control_qubit2, control_qubit1, target_qubit)
+def make_instruction(name, func, qbits):
+    from qiskit import QuantumRegister
+    q = QuantumRegister(qbits)
+
+    from qiskit import QuantumCircuit
+    circuit = QuantumCircuit(q, name=name)
+
+    func(circuit, *[q[i] for i in range(qbits)])
+
+    return circuit.to_instruction()
 
 
-def sum_(circuit: QuantumCircuit, control_qubit1, control_qubit2, target_qubit):
-    circuit.cx(control_qubit1, target_qubit)
-    circuit.cx(control_qubit2, target_qubit)
+def left_carry(circuit: QuantumCircuit, q0, q1, q2, q3):
+    """Upwards carry"""
+    circuit.ccx(q1, q3, q0)
+    circuit.cx(q2, q1)
+    circuit.ccx(q1, q2, q0)
+    circuit.barrier()
+
+
+LCarry = make_instruction('LCarry', left_carry, 4)
+
+
+def right_carry(circuit: QuantumCircuit, q0, q1, q2, q3):
+    """Downwards carry"""
+    circuit.ccx(q1, q2, q3)
+    circuit.cx(q1, q2)
+    circuit.ccx(q0, q2, q3)
+    circuit.barrier()
+
+
+RCarry = make_instruction('RCarry', right_carry, 4)
+
+
+def left_sum(circuit: QuantumCircuit, q0, q1, q2):
+    """Upwards sum"""
+    circuit.cx(q2, q0)
+    circuit.cx(q1, q0)
+    circuit.barrier()
+
+
+LSum = make_instruction('LSum', left_sum, 3)
+
+
+def right_sum(circuit: QuantumCircuit, q0, q1, q2):
+    """Downwards sum"""
+    circuit.cx(q1, q2)
+    circuit.cx(q0, q2)
+    circuit.barrier()
+
+
+RSum = make_instruction('RSum', right_sum, 3)
+
+
+def right_add(circuit: QuantumCircuit, a, b, c):
+    """Downwards add"""
+    assert a.size + 1 <= b.size, 'b register must be at least a.size + 1 long'
+    assert a.size <= c.size, 'c register must be at least a.size long'
+
+    n = a.size - 1
+
+    circuit.barrier()
+
+    for i in range(n):
+        circuit.append(RCarry, [c[i], a[i], b[i], c[i + 1]])
+        # right_carry(circuit, c[i], a[i], b[i], c[i + 1])
+    circuit.append(RCarry, [c[n], a[n], b[n], b[n + 1]])
+    # right_carry(circuit, c[n], a[n], b[n], b[n + 1])
+
+    circuit.cx(a[n], b[n])
+    circuit.barrier()
+
+    circuit.append(RSum, [c[n], a[n], b[n]])
+    # right_sum(circuit, c[n], a[n], b[n])
+    for i in reversed(range(n)):
+        circuit.append(LCarry, [c[i], a[i], b[i], c[i + 1]])
+        # left_carry(circuit, c[i], a[i], b[i], c[i + 1])
+        circuit.append(RSum, [c[i], a[i], b[i]])
+        # right_sum(circuit, c[i], a[i], b[i])
+
+
+def left_add(circuit: QuantumCircuit, a, b, c):
+    """Upwards substract"""
+    assert a.size >= b.size + 1, 'a register must be at least b.size + 1 long'
+    assert b.size <= c.size, 'c register must be at least b.size long'
+
+    n = a.size - 1
+
+    for i in reversed(range(n)):
+        left_sum(circuit, a[i], b[i], )
--- tests/test_Arithmetic.py
+++ tests/test_Arithmetic.py
@@ -2,15 +2,15 @@ import unittest
 
 
 class TestArithmetic(unittest.TestCase):
-    def test_carry(self):
+    def test_left_carry(self):
         from qiskit import QuantumCircuit
         circuit = QuantumCircuit(4)
         circuit.initialize((0, 1), 1)
         circuit.initialize((0, 1), 2)
         circuit.initialize((0, 1), 3)
 
-        from Arithmetic import carry
-        carry(circuit, 1, 2, 3, 0)
+        from Arithmetic import left_carry
+        left_carry(circuit, 0, 1, 2, 3)
         circuit.measure_all()
 
         from qiskit import Aer
@@ -23,14 +23,77 @@ class TestArithmetic(unittest.TestCase):
         self.assertIn('1101', counts)
         self.assertEqual(counts['1101'], 1024)
 
-    def test_sum_(self):
+    def test_right_carry(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(4)
+        circuit.initialize((0, 1), 0)
+        circuit.initialize((0, 1), 1)
+        circuit.initialize((0, 1), 2)
+
+        from Arithmetic import right_carry
+        right_carry(circuit, 0, 1, 2, 3)
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('1011', counts)
+        self.assertEqual(counts['1011'], 1024)
+
+    def test_LCarry(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(4)
+        circuit.initialize((0, 1), 1)
+        circuit.initialize((0, 1), 2)
+        circuit.initialize((0, 1), 3)
+
+        from Arithmetic import LCarry
+        circuit.append(LCarry, [0, 1, 2, 3])
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('1101', counts)
+        self.assertEqual(counts['1101'], 1024)
+
+    def test_RCarry(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(4)
+        circuit.initialize((0, 1), 0)
+        circuit.initialize((0, 1), 1)
+        circuit.initialize((0, 1), 2)
+
+        from Arithmetic import RCarry
+        circuit.append(RCarry, [0, 1, 2, 3])
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('1011', counts)
+        self.assertEqual(counts['1011'], 1024)
+
+    def test_left_sum(self):
         from qiskit import QuantumCircuit
         circuit = QuantumCircuit(3)
         circuit.initialize((0, 1), 1)
         circuit.initialize((0, 1), 2)
 
-        from Arithmetic import sum_
-        sum_(circuit, 1, 2, 0)
+        from Arithmetic import left_sum
+        left_sum(circuit, 0, 1, 2)
         circuit.measure_all()
 
         from qiskit import Aer
@@ -42,6 +105,103 @@ class TestArithmetic(unittest.TestCase):
 
         self.assertIn('110', counts)
         self.assertEqual(counts['110'], 1024)
+
+    def test_right_sum(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(3)
+        circuit.initialize((0, 1), 0)
+        circuit.initialize((0, 1), 1)
+
+        from Arithmetic import right_sum
+        right_sum(circuit, 0, 1, 2)
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('011', counts)
+        self.assertEqual(counts['011'], 1024)
+
+    def test_LSum(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(3)
+        circuit.initialize((0, 1), 1)
+        circuit.initialize((0, 1), 2)
+
+        from Arithmetic import LSum
+        circuit.append(LSum, [0, 1, 2])
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('110', counts)
+        self.assertEqual(counts['110'], 1024)
+
+    def test_RSum(self):
+        from qiskit import QuantumCircuit
+        circuit = QuantumCircuit(3)
+        circuit.initialize((0, 1), 0)
+        circuit.initialize((0, 1), 1)
+
+        from Arithmetic import RSum
+        circuit.append(RSum, [0, 1, 2])
+        circuit.measure_all()
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend)
+        counts = job.result().get_counts(circuit)
+
+        self.assertIn('011', counts)
+        self.assertEqual(counts['011'], 1024)
+
+    @staticmethod
+    def initialize_register_to_number(circuit, register, num: int):
+        from numpy import binary_repr
+        for i, bit in enumerate(binary_repr(num)[::-1]):
+            if bit == '1':
+                circuit.x(register[i])
+
+    def test_right_add(self):
+        from qiskit import QuantumCircuit, QuantumRegister
+        a = QuantumRegister(3)
+        b = QuantumRegister(4)
+        c = QuantumRegister(3)
+        circuit = QuantumCircuit(a, b, c)
+        self.initialize_register_to_number(circuit, a, 5)
+        self.initialize_register_to_number(circuit, b, 7)
+
+        from Arithmetic import right_add
+        right_add(circuit, a, b, c)
+        circuit.measure_all()
+        print(circuit.draw())
+
+        from qiskit import Aer
+        backend = Aer.get_backend('qasm_simulator')
+
+        from qiskit import execute
+        job = execute(circuit, backend, shots=1)
+
+        counts = job.result().get_counts(circuit)
+        res = max(counts, key=lambda x: counts[x])
+        c_res = res[:3]
+        b_res = res[3:7]
+        a_res = res[7:]
+        print(a_res, b_res, c_res)
+        print(int(b_res, 2))
+
+        self.assertEqual(12, int(list(counts.keys())[0][3:7], 2))
 
 
 if __name__ == '__main__':
